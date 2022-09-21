b902292a89d83631be7f9cae585193419d1e27bb
Andre <Andre@DESKTOP-TEVC70B>
2022-09-21 18:02:44 +0300

    Добавьте файлы проекта.

---
+++ Training/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PsychoKids
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+            int num_1 = 0, num_2 = 0;
+ 
+            num_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("First: " + num_1 + ".Second:" + num_2);
+
+
+
+
+
+            
+           
+            
+
+        }
+    }
+}
+
+           
